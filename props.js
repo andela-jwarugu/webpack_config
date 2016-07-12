cf367b33ae09e7ae06e912d96b27d36e78c6812a
andela-jwarugu <joy.warugu@andela.com>
2016-07-12 09:50:42 +0300

    In depth props

--- props.js
+++ props.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var HelloUser = React.createClass({
+var FriendsContainer = React.createClass({
+  getInitialState: function(){
+    return {
+      name: 'Joy Warugu',
+      friends: ['Njerry Werry', 'Jacky Kimani', 'Dama Esther']
+    }
+  },
+  addFriend: function(friend){
+    this.setState({
+      friends: this.state.friends.concat([friend])
+    });
+  },
   render: function(){
     return (
-      <div> Hello, {this.props.name}</div>
+      <div>
+        <h3>Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
     )
   }
 });
 
-ReactDOM.render(<HelloUser name='Joy'/>, document.body);
+var AddFriend = React.createClass({
+  getInitialState: function(){
+    return {
+      newFriend: ''
+    }
+  },
+  updateNewFriend: function(e){
+    this.setState({
+      newFriend: e.target.value
+    });
+  },
+  handleAddNew: function(){
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ''
+    });
+  },
+  render: function(){
+    return (
+      <div>
+        <input tyoe='text' value={this.state.newFriend}
+          onChange={this.updateNewFriend} />
+        <button onClick={this.handleAddNew}> Add Friend
+          </button>
+      </div>
+    );
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends: </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<FriendsContainer/>, document.body);
